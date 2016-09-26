1c24b5e94364fe44142018d5a6fe2ede792183b8
JustFei <JustFei@danwenhaodemacbook-air.local>
2016-09-26 10:57:46 +0800

    Initial Commit

---
+++ ManridyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ManridyApp
+//
+//  Created by JustFei on 16/9/26.
+//  Copyright © 2016年 Manridy.Bobo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ManridyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ManridyApp
+//
+//  Created by JustFei on 16/9/26.
+//  Copyright © 2016年 Manridy.Bobo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
